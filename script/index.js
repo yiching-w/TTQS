180aa5ed9ce7a20a275db60c65868012bf99e511
yiching <doo8409255@gmail.com>
2023-01-26 22:22:56 +0800

    finidh

--- script/index.js
+++ script/index.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
 
   $('.carousel3').slick({
     autoplay: true,
-    autoplaySpeed: 300000,
+    autoplaySpeed: 3000,
     slidesToShow: 4,
     slidesToScroll: 3,
     dots: false,
@@ -41,4 +41,28 @@ $(document).ready(function () {
       }
     ]
   });
+
+  $('.carousel4').slick({
+    slidesToShow: 1,
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000
+  })
+
+  /* Tab */
+  const $aList = $('.lastestNewWrap .lists a');
+  const tabContent = document.querySelectorAll(".lastestNewWrap .tabContent");
+
+  $aList.click(function () {
+    event.preventDefault();
+    $aList.removeClass('active');
+    $(this).addClass('active');
+
+    tabContent.forEach((tab) => {
+      $(tab).removeClass('active');
+      $(`#${$(this).data("tab")}`).addClass('active');
+    })
+  })
+
 })
